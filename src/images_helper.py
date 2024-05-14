97f3b2e81ef496a304b7b4121e47f7f50c2eaac9
Marvin Winkens <m.winkens@fz-juelich.de>
2024-05-14 20:05:53 +0200

    fix range for windows

--- src/images_helper.py
+++ src/images_helper.py
@@ -3,6 +3,6 @@ from PyQt5.QtGui import QImage, QPainter
 CHECKER_IMAGE = QImage("img/checker.png").scaled(16,16)
 
 def drawCheckerImage(qp: QPainter, width: int, height: int):
-    for y in range(0, height, CHECKER_IMAGE.height()):
-        for x in range(0, width, CHECKER_IMAGE.width()):
+    for y in range(height, CHECKER_IMAGE.height()):
+        for x in range(width, CHECKER_IMAGE.width()):
             qp.drawImage(x, y, CHECKER_IMAGE)
