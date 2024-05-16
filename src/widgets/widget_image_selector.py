c89ffebcc24ff954ae75f58d07d956f21f851bfe
AssassinTee <m.winkens@fz-juelich.de>
2024-05-16 16:49:11 +0200

    use default image path for testing, since the dialog doesn't open for no reason

--- src/widgets/widget_image_selector.py
+++ src/widgets/widget_image_selector.py
@@ -11,7 +11,7 @@ class ImageSelectorWidget(QWidget):
         self.setWindowTitle("Image Selector")
         self.setGeometry(100, 100, 400, 300)
 
-        self.image_label = TileClicker()
+        self.image_label = TileClicker(self)
         # self.image_label.setScaledContents(True)
         # self.image_label.setMaximumSize(1024,1024)
 
@@ -25,17 +25,23 @@ class ImageSelectorWidget(QWidget):
         self.setLayout(layout)
 
     def select_image(self):
-        file_dialog = QFileDialog(self)
+        file_dialog = QFileDialog()
         file_dialog.setNameFilter("Images (*.png *.jpg *.jpeg *.bmp)")
         file_dialog.setViewMode(QFileDialog.List)
         file_dialog.setFileMode(QFileDialog.ExistingFile)
 
         if file_dialog.exec_():
             selected_files = file_dialog.selectedFiles()
-            if selected_files:
-                image_path = selected_files[0]
-                pixmap = QPixmap(image_path)
-                if not pixmap.isNull():
-                    self.image_label.setPixmap(pixmap)
-                else:
-                    self.image_label.setText("Invalid image file")
+        else:
+            print("ERROR")
+
+            # debug code
+            selected_files = ["img/grass_main.png"]
+
+        if selected_files:
+            image_path = selected_files[0]
+            pixmap = QPixmap(image_path)
+            if not pixmap.isNull():
+                self.image_label.setPixmap(pixmap)
+            else:
+                self.image_label.setText("Invalid image file")
