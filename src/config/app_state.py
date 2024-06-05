e1bcd0371ca603e493ac246a3318e83eb8c43907
Marvin Winkens <m.winkens@fz-juelich.de>
2024-06-05 17:02:19 +0200

    implement app state class to track important loaded locations and files

---
+++ src/config/app_state.py
@@ -0,0 +1,38 @@
+"""
+Class that handles the application state, sometimes you need to grab information from different points in the application
+"""
+from typing import Optional
+
+from PyQt6.QtGui import QPixmap
+
+from backend.rule_manager import RuleManager
+
+
+class AppState:
+    _instance = None
+
+    def __init__(self):
+        raise RuntimeError("singleton, use instance() instead")
+
+    def _init(self):
+        self.rule_manager: RuleManager = RuleManager()
+        self.main_image_path: Optional[str] = None
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            cls._instance = cls.__new__(cls)
+            cls._instance._init()
+        return cls._instance
+
+    @classmethod
+    def imagePath(cls):
+        return cls.instance().main_image_path
+
+    @classmethod
+    def ruleManager(cls):
+        return cls.instance().rule_manager
+
+    @classmethod
+    def setImagePath(cls, image_path):
+        cls.instance().main_image_path = image_path
